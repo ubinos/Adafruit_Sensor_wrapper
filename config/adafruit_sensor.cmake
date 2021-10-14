0fba1d7c361785c2008c5b8bb5690fb189e56ace
Sungho Park <siminsungho@gmail.com>
2021-10-15 01:23:59 +0900

    update ubinos build scripts

--- config/adafruit_sensor.cmake
+++ config/adafruit_sensor.cmake
@@ -5,6 +5,7 @@
 #
 
 set(INCLUDE__ADAFRUIT_SENSOR TRUE)
+set(PROJECT_UBINOS_LIBRARIES ${PROJECT_UBINOS_LIBRARIES} Adafruit_Sensor_wrapper)
 
 set_cache_default(ADAFRUIT_SENSOR__BASE_DIR "${PROJECT_LIBRARY_DIR}/Adafruit_Sensor" STRING "Adafruit Sensor project base dir")
 
